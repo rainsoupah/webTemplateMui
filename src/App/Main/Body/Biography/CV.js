132e1a604414f535384b63878dac6ad9fe039e69
yutong <yutong.wang.94@gmail.com>
2017-07-05 17:53:52 -0400

    basic profile

--- src/App/Main/Body/Biography/CV.js
+++ src/App/Main/Body/Biography/CV.js
@@ -1,30 +1,55 @@
 import React from 'react'
 import './CV.css'
+import {profileData} from './CVData'
+
+// const styles = {
+//   cv-circle:{
+//     maxWidth: '175px',
+//     border: '#caeaef 10px solid',
+//   },
+// }
 
 const SideBar = () => (
   <nav className="col-sm-3" id="myScrollspy">
     <ul className="nav nav-pills nav-stacked">
-      <li><a href="#section1">Section 1</a></li>
-      <li><a href="#section2">Section 2</a></li>
-      <li><a href="#section3">Section 3</a></li>
+      <li><a href="#section1">Profile</a></li>
+      <li><a href="#section2">Experiences</a></li>
+      <li><a href="#section3">Skills</a></li>
     </ul>
   </nav>
 )
 
-const Section = ({section_id}) => (
+const Traits = () => (
+  <div className="row cv-traits">
+    {
+      profileData.traits.map((trait) => (
+        <div className="col-sm-3">
+          <h3> {trait.type} </h3>
+          <h5> {trait.value} </h5>
+        </div>
+      ))
+    }
+  </div>
+)
+
+const Profile = ({section_id}) => (
   <div id={section_id}>
-    <h1>{section_id}</h1>
-    <p>
-      Try to scroll this section and look at the navigation list while scrolling!Views abode law heard jokes too. Was are delightful solicitude discovered collecting man day. Resolving neglected sir tolerably but existence conveying for. Day his put off unaffected literature partiality inhabiting.
-    </p>
+    <h1>Profile</h1>
+    <h4>Something about me</h4>
+    <hr></hr>
+    <div className="row cv-profile"></div>
+    <div className="row cv-imgAndTraits">
+      <img src={profileData.img} className="img-responsive col-sm-3 img-circle cv-img"/>
+      <div className="col-sm-9"><Traits/></div>
+    </div>
   </div>
 )
 
 const Main = () => (
   <div className="col-sm-9">
-    <Section section_id="section1"/>
-    <Section section_id="section2"/>
-    <Section section_id="section3"/>
+    <Profile section_id="section1"/>
+    <Profile section_id="section2"/>
+    <Profile section_id="section3"/>
   </div>
 )
 
---
+++ src/App/Main/Body/Biography/CVData.js
@@ -0,0 +1,22 @@
+export const profileData = {
+  img: "https://lh3.googleusercontent.com/lwu4CGlB9WdNu2UFcI3FLklqSZGJ4XFbPzSr_iAiYLmJK7vaPRbtPrx3AJeQm-J9Q4TP1Hg24C_e6gxZHUK2ljWNIzw93gdn4Bu4KmXn0hiO6k4EsaWanZ6eTCF8p5v3jlBKmyq63ixMTc29GxK3oZmVjDJU8viCZyW5PDj2guByiIUaNLCVsbI6I4uqMlXR2tH2XTbRTmU8xGpbMpR1zMbfbm9kmcMHsOVkVuTy_Y5jeh6gbAQM4Qm_HQrNJaHyQnEEYQYzcDl9T6R0D_uNWbJcJ4PDmo1tVtgZ9_QDIHWFn_vTkWjC9cgJG3owlLl4S9AxIYNXVDN85Pg5M784JgPXDvaF57tv5jepRF6ETuz3bRElO3Oe3UqH9efDldrsLeHpGKiZm9V_p0wAOx1vZE2xr1LLgnLjgPKhoefrn318zgFXSJ9uMGU_FvRixTN1TJu4UEDAgSG7O-sRCi2REZZt43v7_fB3bKnlo8lP249rCyPx2lz1eOg5SNiL9SM_D3UBxaPC0_n9lFRmkyLXNjUIelo2GK7zh7ukPdQZsQueEo2J7eNgAI09O2ZRCWpnGX-WMTK4dqKnqus9xZ6ufKi9IVEH9kT0tiaJP7SoP6w0rPCdfiUrCXjOuUSSDFhZZquasT9MnhmOFHHZd6PRihZCKJLcIT3Z54TnCd4C9w=w711-h798-no",
+
+  traits: [
+    {
+      type: "Name: ",
+      value: "Yutong Wang",
+    },
+    {
+      type: "Education: ",
+      value: "Bachelor of Computing and Financial Management, University of Waterloo",
+    },
+    {
+      type: "Location: ",
+      value: "Canada, close to North Pole, Earth",
+    },
+    {
+      type: "Age: ",
+      value: "22.79 years",
+    }
+  ]
+}
